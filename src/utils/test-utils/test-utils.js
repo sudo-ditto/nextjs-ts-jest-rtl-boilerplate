f45a1d7119ed7dd9a7a5cf788f49a4d5490743b0
Loreta Krasteva <52462274+loretta-arineva@users.noreply.github.com>
2022-02-03 11:42:17 +0200

    new: PRO-1 (Create a starting template dor new projects) - Small touches to package.json, tsconfig.json and eslintrc

--- src/utils/test-utils/test-utils.js
+++ src/utils/test-utils/test-utils.js
@@ -6,8 +6,7 @@ const Providers = ({ children }) => {
     return children;
 };
 
-const customRender = (ui, options) =>
-    render(ui, { wrapper: Providers, ...options });
+const customRender = (ui, options) => render(ui, { wrapper: Providers, ...options });
 
 // re-export everything
 export * from '@testing-library/react';
